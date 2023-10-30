62f13b5b4b84bae356be2676dbf2d9c4dafeec14
HIEU TRUONG <aceblack0003@gmail.com>
2023-10-30 18:40:46 +0700

    first commit

---
+++ components/app.footer.tsx
@@ -0,0 +1,13 @@
+'use client'
+const AppFooter=()=>{
+    return(
+        <div style={{
+            background:'#ddd',
+            textAlign:'center',
+            padding:'5px 0',
+        }}>
+            NEXT JS and HoidanIt
+        </div>
+    )
+}
+export default AppFooter
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
+'use client'
+
 import './globals.css'
 import { Inter } from 'next/font/google'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AppHeader from '../../components/app.header';
+import AppFooter from '../../components/app.footer';
+import Container from 'react-bootstrap/Container';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +21,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AppHeader />
+        <Container>
+          {children}
+        </Container>
+        <AppFooter />
+      </body>
     </html>
   )
 }
